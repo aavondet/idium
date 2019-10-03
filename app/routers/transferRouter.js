0dcf2cc2960e420dd6ffbe4de9cbb1fbb4bee688
William Zhang <wz2@andrew.cmu.edu>
2019-10-03 02:13:43 -0700

    transfer works...

--- app/routers/transferRouter.js
+++ app/routers/transferRouter.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var ilp = require('./ilp-plugin');
-var spsp = require('ilp-protocol-spsp');
-var Account = require('../models/account');
+var request = require('request');
+var account = require('../models/account');
 
 const transferRouter = express.Router();
 transferRouter.use(bodyParser.json());
 
 transferRouter.route('/:id/:tid/:amount')
-.get(async (req, res, next) => {
-    account.findById(req.params.id)
+.get((req, res, next) => {
+    account.findOne({'username': req.params.id})
     .then((source) => {
-      account.findById(req.params.tid)
-      .then(async (target) => {
-        i = ilp({'user': source.ilp_name, 'secret': source.secret, 'endpoint': 'btp+wss://rs3.xpring.dev/ilp/btp' })
-        await SPSP.pay(i, { receiver: target.ilp_pointer, sourceAmount: amount })
+      account.findOne({'username': req.params.tid})
+      .then((target) => {
+        var urls = "https://rs3.xpring.dev/accounts/" + source.ilp_name + "/payments";
+        var bodyd = JSON.stringify({
+          "receiver": target.ilp_pointer,
+          "source_amount": req.params.amount
+        })
+
+        request.post({
+          url: urls,
+          body: bodyd,
+          headers: { 
+            'Authorization': 'Bearer ' + source.ilp_name + ':' + source.secret,
+            'Content-Type': 'application/json'
+          },
+          method: 'POST'
+        }, function (err, result) {
+          res.send(result.body)
+        });
       }, (err) => next(err))
       .catch((err) => next(err));
     }, (err) => next(err))
