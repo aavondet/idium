48c1678f3a497963bbd1d68dcd576366cad4151b
myusername <avondeta3.14@gmail.com>
2019-10-02 22:17:15 -0700

    Added server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+// favicon
+//app.use(favicon(path.join(__dirname,'public','favicon.ico')));
+
+// Routers
+const accountRouter = require('./app/routers/accountRouter'); 
+const articleRouter = require('./app/routers/articleRouter');
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+var db_user = process.env.DB_USER;
+var db_pass = process.env.DB_PASS;
+
+// Connect to mongoDB
+// If using local server: localConfig.DB
+// If using MongoDB Atlas: 
+// mongodb+srv://<username>:<password>@cluster0-c14lf.gcp.mongodb.net/test?retryWrites=true
+mongoose.connect('mongodb+srv://' + db_user + ':' 
+    + db_pass + '@cluster0-c14lf.gcp.mongodb.net/test?retryWrites=true')
+.then(() => {
+    console.log('Connected to Server');
+})
+.catch((err) => console.log(err));
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname+'/public'));
+app.use('/account', accountRouter);
+app.use('/article', articleRouter);
+
+app.get('/', (req, res, next) => {
+    res.sendFile('./public/index.html');
+});
+
+app.listen(PORT);
+console.log("listening on port:" + PORT);
